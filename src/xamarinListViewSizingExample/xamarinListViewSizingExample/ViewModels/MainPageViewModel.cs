e22148cc793c56a9de6e8414d83be1cb0ec9ebba
hobber89 <dotteldu@gmail.com>
2023-09-11 21:46:41 +0200

    MainPage: added FillListsButton and two ListViews.

--- src/xamarinListViewSizingExample/xamarinListViewSizingExample/ViewModels/MainPageViewModel.cs
+++ src/xamarinListViewSizingExample/xamarinListViewSizingExample/ViewModels/MainPageViewModel.cs
@@ -1,11 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace xamarinListViewSizingExample.ViewModels
 {
     internal class MainPageViewModel : ViewModelBase
     {
+        public ButtonCommandBinding FillListsButtonCommandBinding { get; private set; }
+
         public string PageTitle => "This page will consist of several ListViews...";
+
+        public MainPageViewModel()
+        {
+            FillListsButtonCommandBinding = new ButtonCommandBinding(fillListsButtonCommand, true);
+        }
+
+        private void fillListsButtonCommand()
+        {
+            //TODO
+        }
     }
 }
