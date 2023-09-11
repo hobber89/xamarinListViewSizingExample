66aa145af3f31b8ffc52ee5f82383005310cdf9b
hobber89 <dotteldu@gmail.com>
2023-09-11 21:42:05 +0200

    Added ButtonCommandBinding.

---
+++ src/xamarinListViewSizingExample/xamarinListViewSizingExample/ViewModels/ButtonCommandBinding.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace xamarinListViewSizingExample.ViewModels
+{
+    internal class ButtonCommandBinding : ICommand
+    {
+        private readonly Action _handler;
+
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+            set
+            {
+                if (value != _isEnabled)
+                {
+                    _isEnabled = value;
+                    if (CanExecuteChanged != null)
+                    {
+                        CanExecuteChanged(this, EventArgs.Empty);
+                    }
+                }
+            }
+        }
+        private bool _isEnabled;
+
+        public ButtonCommandBinding(Action handler)
+        {
+            _handler = handler;
+        }
+
+        public ButtonCommandBinding(Action handler, bool isEnabled)
+        {
+            _handler = handler;
+            _isEnabled = isEnabled;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return IsEnabled;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _handler();
+        }
+    }
+}
