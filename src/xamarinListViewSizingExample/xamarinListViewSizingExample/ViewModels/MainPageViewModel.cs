8c29b4d7173c43324c88c1fb7c099d20e4109db1
hobber89 <dotteldu@gmail.com>
2023-09-11 21:22:31 +0200

    Started to define content of MainPage.

---
+++ src/xamarinListViewSizingExample/xamarinListViewSizingExample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xamarinListViewSizingExample.ViewModels
+{
+    internal class MainPageViewModel : ViewModelBase
+    {
+        public string PageTitle => "This page will consist of several ListViews...";
+    }
+}
--- src/xamarinListViewSizingExample/xamarinListViewSizingExample/Views/MainPage.xaml.cs
+++ src/xamarinListViewSizingExample/xamarinListViewSizingExample/Views/MainPage.xaml.cs
@@ -5,14 +5,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using xamarinListViewSizingExample.ViewModels;
 
 namespace xamarinListViewSizingExample.Views
 {
     public partial class MainPage : ContentPage
     {
+        private MainPageViewModel _viewModel;
+
         public MainPage()
         {
             InitializeComponent();
+
+            _viewModel = new MainPageViewModel();
+
+            BindingContext = _viewModel;
         }
     }
 }
