e3d7f60491b37e59e4572af92703218b7ee95442
hobber89 <dotteldu@gmail.com>
2023-09-11 21:15:49 +0200

    Moved MainPage to new Views directory.

--- src/xamarinListViewSizingExample/xamarinListViewSizingExample/Views/MainPage.xaml.cs
+++ src/xamarinListViewSizingExample/xamarinListViewSizingExample/Views/MainPage.xaml.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace xamarinListViewSizingExample
+namespace xamarinListViewSizingExample.Views
 {
     public partial class MainPage : ContentPage
     {
