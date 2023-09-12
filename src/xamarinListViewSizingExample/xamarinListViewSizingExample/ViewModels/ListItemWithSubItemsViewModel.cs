af1b0e79cac98781e99d256bec671536af20811c
hobber89 <dotteldu@gmail.com>
2023-09-12 21:55:02 +0200

    MainPage: added ListView with fix height per item.

--- src/xamarinListViewSizingExample/xamarinListViewSizingExample/ViewModels/ListItemWithSubItemsViewModel.cs
+++ src/xamarinListViewSizingExample/xamarinListViewSizingExample/ViewModels/ListItemWithSubItemsViewModel.cs
@@ -19,6 +19,8 @@ namespace xamarinListViewSizingExample.ViewModels
         }
         private double _subItemsListHeight = 1;
 
+        public double SubItemsListHeightWithFixHeightPerItem => SubItems.Count * 60;
+
         private ListItemWithSubItemsModel _model;
         private Dictionary<Guid, double> _viewsHeightMap = new Dictionary<Guid, double>();
 
