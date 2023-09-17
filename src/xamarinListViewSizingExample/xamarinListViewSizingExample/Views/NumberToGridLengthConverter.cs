1c94fd49e973fb117e6660ff6b37d44f4e2ff153
hobber89 <dotteldu@gmail.com>
2023-09-17 20:22:19 +0200

    Added NumberToGridLengthConverter.

---
+++ src/xamarinListViewSizingExample/xamarinListViewSizingExample/Views/NumberToGridLengthConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace xamarinListViewSizingExample.Views
+{
+    internal class NumberToGridLengthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is double numberValue))
+            {
+                throw new Exception($"Type {value.GetType().FullName} cannot be converted to GridLenght");
+            }
+
+            return new GridLength(numberValue, GridUnitType.Absolute);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is GridLength gridLength))
+            {
+                throw new Exception($"Type {value.GetType().FullName} is not GridLenght");
+            }
+
+            return gridLength.Value;
+        }
+    }
+}
